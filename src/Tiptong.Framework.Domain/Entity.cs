16a07a82ebf19fea9246b8f575dc60ee93c839d3
kkadir <kadir.kilicoglu@gmail.com>
2019-01-21 10:39:22 -0500

    Add object overrides on entity

--- src/Tiptong.Framework.Domain/Entity.cs
+++ src/Tiptong.Framework.Domain/Entity.cs
@@ -1,6 +1,7 @@
 namespace Tiptong.Framework.Domain
 {
     using System;
+    using System.Reflection;
     using Tiptong.Framework.Domain.Contracts;
     using Tiptong.Framework.Domain.Factories;
 
@@ -31,6 +32,58 @@ namespace Tiptong.Framework.Domain
         public bool ValidateIdentity(T identity)
         {
             return _identityStrategy.ValidateIdentity(identity);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Entity<T>))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            var other = (Entity<T>)obj;
+            if (!HasValidIdentity() || !other.HasValidIdentity())
+            {
+                return false;
+            }
+
+            if (!GetType().GetTypeInfo().IsAssignableFrom(other.GetType())
+                || !other.GetType().GetTypeInfo().IsAssignableFrom(GetType()))
+            {
+                return false;
+            }
+
+            return Id.Equals(other.Id);
+        }
+
+        public static bool operator ==(Entity<T> left, Entity<T> right)
+        {
+            if (Equals(left, null))
+            {
+                return Equals(right, null);
+            }
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Entity<T> left, Entity<T> right)
+        {
+            return !(left == right);
+        }
+
+        public override int GetHashCode()
+        {
+            if (HasValidIdentity())
+            {
+                return Id.GetHashCode();
+            }
+
+            return 0;
         }
     }
 }
