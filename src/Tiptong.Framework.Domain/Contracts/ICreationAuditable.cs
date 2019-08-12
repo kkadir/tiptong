c344fdc37a9a9ac86885d5dd31b29486317e812e
kkadir <kadir.kilicoglu@aurea.com>
2019-08-12 03:33:43 +0300

    Refactor ICreationAuditable.

--- src/Tiptong.Framework.Domain/Contracts/ICreationAuditable.cs
+++ src/Tiptong.Framework.Domain/Contracts/ICreationAuditable.cs
@@ -1,11 +1,27 @@
 namespace Tiptong.Framework.Domain.Contracts
 {
     using System;
-    
-    public interface ICreationAuditable<T>
-    {
-        T CreatedBy { get; set; }
 
-        DateTime CreationDate { get; set; }
+    /// <summary>
+    /// Enables auditing of the creation properties of an <see cref="Entity{T}"/>.
+    /// </summary>
+    /// <typeparam name="TUserId">The primary key type of the user who created the entity.</typeparam>
+    public interface ICreationAuditable<TUserId>
+    {
+
+        /// <summary>
+        /// Gets or sets the user who created the entity.
+        /// </summary>
+        /// <value> The user who created the entity. </value>
+        TUserId CreatedBy { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets the creation time stamp of the entity.
+        /// </summary>
+        /// <value>
+        /// The creation time stamp of the entity.
+        /// </value>
+        DateTime CreationTimeStamp { get; set; }
     }
 }
